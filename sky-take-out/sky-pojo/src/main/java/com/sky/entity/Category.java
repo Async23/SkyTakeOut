f8a595002a8a193c2f2593eb9ce1e5e30bf42fbc
Async <gearrr005@gmail.com>
2023-04-20 02:22:03 +0800

    分类分页查询，最后操作时间格式完善

--- sky-take-out/sky-pojo/src/main/java/com/sky/entity/Category.java
+++ sky-take-out/sky-pojo/src/main/java/com/sky/entity/Category.java
@@ -1,9 +1,11 @@
 package com.sky.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
@@ -17,27 +19,29 @@ public class Category implements Serializable {
 
     private Long id;
 
-    //类型: 1菜品分类 2套餐分类
+    // 类型: 1菜品分类 2套餐分类
     private Integer type;
 
-    //分类名称
+    // 分类名称
     private String name;
 
-    //顺序
+    // 顺序
     private Integer sort;
 
-    //分类状态 0标识禁用 1表示启用
+    // 分类状态 0标识禁用 1表示启用
     private Integer status;
 
-    //创建时间
+    // 创建时间
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createTime;
 
-    //更新时间
+    // 更新时间
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updateTime;
 
-    //创建人
+    // 创建人
     private Long createUser;
 
-    //修改人
+    // 修改人
     private Long updateUser;
 }
