a5dc8704004cd193f3c45634b02e7a44421eecb7
Async <gearrr005@gmail.com>
2023-04-20 01:52:31 +0800

    Revert "已完成：employee 修改密码，套餐分页查询"
    This reverts commit 3fc036eb0474c5c6e2379c51232eda818865b618.

--- sky-take-out/sky-server/src/main/java/com/sky/service/impl/CategoryServiceImpl.java
+++ sky-take-out/sky-server/src/main/java/com/sky/service/impl/CategoryServiceImpl.java
@@ -37,20 +37,19 @@ public class CategoryServiceImpl implements CategoryService {
         }
 
         // 分类名称 与 分类类型 任一有效，则忽略分页参数，根据相应条件查询
-        String trimName = categoryPageQueryDTO.getName() == null ? null : categoryPageQueryDTO.getName().trim();
-        boolean nameNotValid = trimName == null || "".equals(trimName);
+        boolean nameNotValid = categoryPageQueryDTO.getName() == null || "".equals(categoryPageQueryDTO.getName().trim());
         boolean typeNotValid = categoryPageQueryDTO.getType() == null;
         if (!(nameNotValid && typeNotValid)) {
-            List<Category> categoryList = categoryMapper.selectByNameOrType(trimName, categoryPageQueryDTO.getType());
+            List<Category> categoryList = categoryMapper.selectByNameOrType(categoryPageQueryDTO.getName().trim(), categoryPageQueryDTO.getType());
             return new PageResult(categoryList.size(), categoryList);
         }
 
-        // 分类名称 与 分类类型 无效，分页参数无效
+        // 分类名称与分类类型均为空，分页参数为空
         if (categoryPageQueryDTO.getPage() == null || categoryPageQueryDTO.getPageSize() == null) {
             throw new BaseException(MessageConstant.CATEGORY_QUERY_PAGE_ILLEGAL_ARGUMENT);
         }
 
-        // 分类名称 与 分类类型 无效，分页参数有效，常规分页查询
+        // 分类名称与分类类型均为空，分页参数不为空，常规分页查询
         PageHelper.startPage(categoryPageQueryDTO.getPage(), categoryPageQueryDTO.getPageSize());
         Page<Category> categoryPage = (Page<Category>) categoryMapper.selectAll();
         return new PageResult(categoryPage.getTotal(), categoryPage.getResult());
