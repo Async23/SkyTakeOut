6d22d6ff1fb09da30510cecb0249f6a0e6b0cb3b
Async23 <gearrr005@gmail.com>
2023-04-26 15:39:09 +0800

    fix:纠正 user/SetmealController 的根据分类 id 查询套餐列表(selectByCategoryId 方法)中的的错误：xml 文件中 category_id 单词拼写错误

--- sky-take-out/sky-server/src/main/java/com/sky/controller/user/DishController.java
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/user/DishController.java
@@ -1,4 +1,42 @@
 package com.sky.controller.user;
 
+import com.sky.result.Result;
+import com.sky.service.DishService;
+import com.sky.vo.DishVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * C 端菜品管理
+ *
+ * @author Async_
+ */
+@Slf4j
+@RequestMapping("/user/dish")
+@Api(tags = "C 端菜品管理 φ(*￣0￣)")
+@RestController("userDishController")
 public class DishController {
+    @Autowired
+    private DishService dishService;
+
+    /**z
+     * 根据 categoryId 查询所有菜品(dish 表中)，新增套餐时使用
+     *
+     * @param categoryId
+     * @return
+     */
+    @GetMapping("/list")
+    @ApiOperation("根据 categoryId 查询所有菜品 `(*>﹏<*)′")
+    public Result selectAllByCategoryId(Long categoryId) {
+        List<DishVO> dishVOList = dishService.selectAllByCategoryId(categoryId);
+
+        return Result.success(dishVOList);
+    }
 }
