a9c185b76ca343f2d949c11fbcff194d401689c8
Async23 <gearrr005@gmail.com>
2023-04-29 19:46:11 +0800

    add:userOrderController 中，查询订单详情 功能完成(selectById 方法)

--- sky-take-out/sky-server/src/main/java/com/sky/controller/user/OrderController.java
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -8,6 +8,7 @@ import com.sky.result.Result;
 import com.sky.service.OrderService;
 import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderSubmitVO;
+import com.sky.vo.OrderVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -69,4 +70,19 @@ public class OrderController {
 
         return Result.success(ordersPageResult);
     }
+
+    /**
+     * 根据 id 查询订单详情
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/orderDetail/{id}")
+    @ApiOperation("查询订单详情")
+    public Result<OrderVO> selectById(@PathVariable Long id) {
+        OrderVO orderVO = orderService.selectById(id);
+
+        return Result.success(orderVO);
+    }
+
 }
